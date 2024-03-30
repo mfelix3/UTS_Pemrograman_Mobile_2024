4f68de9a05aa221a4557b1b99d42ce91ddb95e55
Michael Felix <michalefelix020@gmail.com>
2024-03-30 20:49:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/movies/Item.kt
@@ -0,0 +1,21 @@
+package com.example.movies
+
+data class Item(val title: String, val imageUrl: String, val linkUrl: String)
+val itemList = listOf(
+    Item("Judul Film 1", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.liputan6.com%2Fcitizen6%2Fread%2F5536290%2Ftop-3-mengapa-avatar-the-last-airbender-2024-sukses-besar-ini-alasannya&psig=AOvVaw1ndyxk7B2cSE6BGPxzQhFK&ust=1711643239553000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCID_3ovulIUDFQAAAAAdAAAAABAM",
+        "https://www.imdb.com/title/tt9018736/"),
+    Item("Judul Film 2", "https://example.com/image2.jpg", "https://example.com/link2"),
+    Item("Judul Film 3", "https://example.com/image3.jpg", "https://example.com/link3"),
+    Item("Judul Film 4", "https://example.com/image4.jpg", "https://example.com/link4"),
+    Item("Judul Film 5", "https://example.com/image5.jpg", "https://example.com/link5"),
+    Item("Judul Film 6", "https://example.com/image6.jpg", "https://example.com/link6"),
+    Item("Judul Film 7", "https://example.com/image7.jpg", "https://example.com/link7"),
+    Item("Judul Film 8", "https://example.com/image8.jpg", "https://example.com/link8"),
+    Item("Judul Film 9", "https://example.com/image9.jpg", "https://example.com/link9"),
+    Item("Judul Film 10", "https://example.com/image10.jpg", "https://example.com/link10"),
+    Item("Judul Film 11", "https://example.com/image11.jpg", "https://example.com/link11"),
+    Item("Judul Film 12", "https://example.com/image12.jpg", "https://example.com/link12"),
+    Item("Judul Film 13", "https://example.com/image13.jpg", "https://example.com/link13"),
+    Item("Judul Film 14", "https://example.com/image14.jpg", "https://example.com/link14"),
+    Item("Judul Film 15", "https://example.com/image15.jpg", "https://example.com/link15"),
+)
---
+++ app/src/main/java/com/example/movies/RecyclerViewAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.movies
+
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(private val itemList: List<Item>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val textViewTitle: TextView = itemView.findViewById(R.id.textViewTitle)
+
+        init {
+            imageView.setOnClickListener {
+                val linkUrl = itemList[adapterPosition].linkUrl
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkUrl))
+                itemView.context.startActivity(intent)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemList[position]
+        Picasso.get().load(currentItem.imageUrl).into(holder.imageView)
+        holder.textViewTitle.text = currentItem.title
+    }
+
+    override fun getItemCount() = itemList.size
+}
